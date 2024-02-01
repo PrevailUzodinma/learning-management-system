84b183242e0c41e16a8ae9e1dd35fb52ce520995
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-02-01 12:14:09 +0100

    added a method verifylogin that will serve as my User authentication for when users login

--- lms.js
+++ lms.js
@@ -1,8 +1,19 @@
-class User{
-    constructor(id, password, role){
-        this.id = id;
-        this.password = password;
-        this.role = role;
-        this.loginStatus = false;
+class User {
+  constructor(id, password, role) {
+    this.id = id;
+    this.password = password;
+    this.role = role;
+    this.loginStatus = false;
+    this.registerDate = new Date();
+  }
+
+  verifylogin(enteredID, enteredPassword) {
+    if (enteredID === this.id && enteredPassword === this.password) {
+      console.log(`Success! Access granted`);
+    } else {
+      {
+        `Incorrect login details! Access denied, please check login details.`;
+      }
     }
+  }
 }
